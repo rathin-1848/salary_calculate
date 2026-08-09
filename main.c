f3cb6a323c159a2d9cf6efdc2d2e729f0984d107
Rathin Baidya <rathin.baidya2026@gmail.com>
2026-08-09 09:56:11 +0530

    Add salary calculation program in main.c
    Implement salary calculation with DA, HRA, TA, and tax.

---
+++ main.c
@@ -0,0 +1,30 @@
+/*Salary Calculation*/
+
+#include<stdio.h>
+
+int main()
+{
+    char name[100];
+    float da,hra,ta,tax,salary,net;
+    
+    printf("Enter your name: ");
+    scanf("%[^\n]", name);
+    
+    printf("Enter your salary: ");
+    scanf("%f", &salary);
+    
+    da = (salary*20)/100;
+    hra = (salary*40)/100;
+    ta = (salary*10)/100;
+    tax = ((salary+da+hra+ta)*10)/100;
+    net = (salary+da+hra+ta)-tax;
+    
+    
+    printf("See your salary details.\n");
+    printf("%s's DA is: %.2f\n", name,da);
+    printf("Yours HRA is: %.2f\n", hra);
+    printf("Yours TA is: %.2f\n", ta);
+    printf("Yours TAX is: %.2f\n", tax);
+    printf("Yours total salary respect to %.2f: %.2f", name,salary,net);
+    return 0;
+}
