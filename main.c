268fc00bcbe1a61200c453b2b6cb889a7b194781
Rathin Baidya <rathin.baidya2026@gmail.com>
2026-08-17 08:45:15 +0530

    Fix scanf format for name input

--- main.c
+++ main.c
@@ -6,20 +6,20 @@ int main()
 {
     char name[100];
     float da,hra,ta,tax,salary,net;
-    
+
     printf("Enter your name: ");
-    scanf("%[\^n]", name);
-    
+    scanf("%[^\n]", name);
+
     printf("Enter your salary: ");
     scanf("%f", &salary);
-    
+
     da = (salary*20)/100;
     hra = (salary*40)/100;
     ta = (salary*10)/100;
     tax = ((salary+da+hra+ta)*10)/100;
     net = (salary+da+hra+ta)-tax;
-    
-    
+
+
     printf("See your salary details.\n");
     printf("%s's DA is: %.2f\n", name,da);
     printf("Yours HRA is: %.2f\n", hra);
